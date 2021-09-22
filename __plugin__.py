d0d0ae4e32b01c97a5c1c0f1eba504f995370a60
AymericBr <AymericBr@users.noreply.github.com>
2021-09-22 17:02:34 +0200

    credits, glTF conversion and export progress

--- __plugin__.py
+++ __plugin__.py
@@ -1,3 +1,16 @@
 id="{67b4099b-8e5f-434b-a842-fb1b7ef7e1f7}"
-version="1.0.1.0"
+
+# From https://semver.org/
+# Semantic Versioning 2.0.0
+# Summary
+
+# Given a version number MAJOR.MINOR.PATCH, increment the:
+
+#     MAJOR version when you make incompatible API changes,
+#     MINOR version when you add functionality in a backwards compatible manner, and
+#     PATCH version when you make backwards compatible bug fixes.
+
+# Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+
+version="1.2.0.0" # Must be x.y.z.0
 title="CatalogueExporter"
